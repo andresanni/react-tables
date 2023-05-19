27926b733e540d879b19e028e6aa64e624182b6e
Andres Anni <andres.anni1985@gmail.com>
2023-05-18 22:27:06 -0300

    First Commit

---
+++ src/components/FilteringTable.js
@@ -0,0 +1,84 @@
+import React, { useMemo } from "react";
+import { useTable, useGlobalFilter } from "react-table";
+import MOCK_DATA from "./MOCK_DATA.json";
+import { COLUMNS } from "./columns";
+import './BasicTable.css'
+import { GlobalFilter } from "./GlobalFilter";
+
+const FilteringTable = () => {
+  const columns = useMemo(() => COLUMNS, []);
+  const data = useMemo(() => MOCK_DATA, []);
+
+  
+  const tableInstance = useTable({ columns, data }, useGlobalFilter);
+
+  const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow, footerGroups,state,setGlobalFilter } =
+    tableInstance;
+
+  const {globalFilter} = state; 
+
+    return (
+    <div className="container">    
+    <GlobalFilter filter={globalFilter} setFilter={setGlobalFilter} />
+    <table {...getTableProps()}>
+      <thead>
+        {// Loop over the header rows
+        headerGroups.map(headerGroup => (
+          // Apply the header row props
+          <tr {...headerGroup.getHeaderGroupProps()}>
+            {// Loop over the headers in each row
+            headerGroup.headers.map(column => (
+              // Apply the header cell props
+              <th {...column.getHeaderProps()}>
+                {// Render the header
+                column.render('Header')}
+              </th>
+            ))}
+          </tr>
+        ))}
+      </thead>
+      {/* Apply the table body props */}
+      <tbody {...getTableBodyProps()}>
+        {// Loop over the table rows
+        rows.map(row => {
+          // Prepare the row for display
+          prepareRow(row)
+          return (
+            // Apply the row props
+            <tr {...row.getRowProps()}>
+              {// Loop over the rows cells
+              row.cells.map(cell => {
+                // Apply the cell props
+                return (
+                  <td {...cell.getCellProps()}>
+                    {// Render the cell contents
+                    cell.render('Cell')}
+                  </td>
+                )
+              })}
+            </tr>
+          )
+        })}
+      </tbody>
+      <tfoot>
+        {
+          footerGroups.map(footerGroup =>(
+            <tr {...footerGroup.getFooterGroupProps()}>
+              {
+                footerGroup.headers.map(column=>(
+                  <td {...column.getFooterProps}>
+                    {column.render("Footer")}
+                  </td>
+                ))
+              }
+            </tr>
+          ))
+        }
+      </tfoot>
+    </table>
+    </div>
+    
+    );
+};
+
+export default FilteringTable;
---
+++ src/components/columns.js
@@ -0,0 +1,92 @@
+import { format } from "date-fns";
+
+export const COLUMNS = [
+  {
+    Header: "Id",
+    accessor: "id",
+    Footer:"Id"
+
+  },
+  {
+    Header: "First Name",
+    accessor: "first_name",
+    Footer:"First Name"
+  },
+  {
+    Header: "Last Name",
+    accessor: "last_name",
+    Footer:"Last Name"
+  },
+  {
+    Header: "Email",
+    accessor:"email",
+    Footer:"Email"
+  },
+  {
+    Header: "Date of Birth",
+    accessor:"date_of_birth",
+    Footer:"Date of Birth",
+    Cell: ({value}) =>{return format(new Date(value),"dd/MM/yyyy")}
+  },
+  {
+    Header: "Age",
+    accessor:"age",
+    Footer:"Age"
+  },
+  {
+    Header: "Country",
+    accessor:"country",
+    Footer:"Country"
+  },
+  {
+    Header: "Phone",
+    accessor:"phone",
+    Footer:"Phone"
+  },
+];
+
+export const GROUPED_COLUMNS=[
+  {
+    Header: "Id",
+    accessor: "id",
+    Footer:"Id"
+
+  },
+  {
+    Header:"Name",
+    Footer:"Name",
+    columns:[
+      {
+        Header: "First Name",
+        accessor: "first_name",
+        Footer:"First Name"
+      },
+      {
+        Header: "Last Name",
+        accessor: "last_name",
+        Footer:"Last Name"
+      }
+    ]
+  },
+  {
+    Header:"Info",
+    Footer:"Info",
+    columns:[
+      {
+        Header: "Age",
+        accessor:"age",
+        Footer:"Age"
+      },
+      {
+        Header: "Country",
+        accessor:"country",
+        Footer:"Country"
+      },
+      {
+        Header: "Phone",
+        accessor:"phone",
+        Footer:"Phone"
+      }
+    ]
+  }
+]
